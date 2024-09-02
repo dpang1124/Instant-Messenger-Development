0e6eeec0385f4aec97ed28105cc79c8a8b21bfe4
dpang1124 <dpang1124@gmail.com>
2024-09-02 13:20:00 -0400

    Inserting My Code

--- src/pack.c
+++ src/pack.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <sys/cdefs.h>
 
 #include "serialize.h"
 
@@ -11,7 +12,132 @@
  * Returns the packet type for valid input, or -1 for invalid input.
  */
 int pack(void *packed, char *input) {
-    return -1;
+
+    //checks if the string is just ascii spaces (error)
+    int check = 0;
+    for(int i=0; i<strlen(input); i++){
+        if(input[i]!=' '){
+            check = 1;
+        }
+    }
+
+    if(check==0){return -1;}
+
+
+    if(strlen(input)>MAX_MESSAGE_SIZE){return -1;}
+    
+    char *ubit = "dpang3\0\0\0\0\0\0\0\0\0\0";
+    
+    
+    //STATUS
+    int track;
+        if(input[0]=='/'&&input[1]=='m'&&input[2]=='e'){
+            if(input[3]!=' '){return -1;}
+
+            //packet type 4
+            *(int *)packed = STATUS;
+            //ubit 20
+            for(int i=0; i<NAME_SIZE; i++){
+                *(char *)(packed + (sizeof(int)+i)) = ubit[i];
+            }
+  
+            //status length 28 
+            for(int i=3; i<strlen(input); i++){
+                if(input[i]!=' '){
+                    track = i;
+                    break;
+                }
+            }
+
+            *(size_t *)(packed + (5* sizeof(int))) = strlen(input) - track;
+
+            //random size_t = 0 end of array signifier 36
+            *(size_t *)(packed + (7* sizeof(int))) = 0;
+
+            //status starts at 36 ends at however long the message is
+            for(int i = track; i<strlen(input); i++){
+                *(char *)(packed + (36 + (i-track))) = input[i];
+            }
+           
+            
+            
+    }
+        //LABELED
+        //"@david   hello" track = 6, track2 = 9, strlen = 14, message = 5 
+        else if(input[0]=='@'){
+            for(int i=0; i<strlen(input); i++){
+                if(input[i]==' '){
+                    track = i;
+                    break;
+                }
+            }
+
+            if((track-1) > NAME_SIZE){return -1;}
+            
+            //packet type 4
+            *(int *)packed = LABELED;
+            //ubit 20
+            for(int i=0; i<NAME_SIZE; i++){
+                *(char *)(packed + (sizeof(int)+i)) = ubit[i];
+            }
+            //message length
+            int track2;
+            for(int i=track; i<strlen(input); i++){
+                if(input[i]!=' '){
+                    track2 = i;
+                    break;
+                }
+            }
+            *(size_t *)(packed + (5*sizeof(int))) = (strlen(input) - (track2));
+            //target length
+            *(size_t *)(packed + (7*sizeof(int))) = (track-1);
+            //random size_t thingy 0
+            *(size_t *)(packed + (9*sizeof(int))) = 0;
+            //message
+            for(int i=track2; i<strlen(input); i++){
+                *(char *)(packed + (11*sizeof(int)+(i-track2))) = input[i];
+            }
+            //target
+            for(int i=1; i<track; i++){
+                *(char *)(packed + ((11*sizeof(int)) + (strlen(input)-track2) + (i-1))) = input[i];
+            }
+           
+            
+        }
+
+        
+        //STATISTICS
+        else if(input[0]=='/'&&input[1]=='s'&&input[2]=='t'&&input[3]=='a'&&input[4]=='t'&&input[5]=='s'){
+            *(int *)packed = STATISTICS;
+            for(int i=0; i<NAME_SIZE; i++){
+                *(char *)(packed + (sizeof(int)+i)) = ubit[i];
+            }
+            
+            return *(int *)packed;
+        }
+        //MESSAGE
+        else{
+
+            
+            //packet type 4
+            *(int *)packed = MESSAGE;
+            //ubit 20
+            for(int i=0; i<NAME_SIZE; i++){
+                *(char *)(packed+ (sizeof(int)+i)) = ubit[i];  
+            }
+            //message length 28 after line below
+            *(size_t *)(packed+(5*(sizeof(int)))) = strlen(input);
+            //random size_t thingy 36 after line below
+            *(size_t *)(packed + 7*(sizeof(int))) = 0;
+            //message starts at 36 and ends at however long the message is
+            for(int i=0; i<strlen(input); i++){
+                *(char *)(packed + (36 + i)) = input[i];
+            }
+            return *(int *)packed;
+        }
+
+    
+        return -1;
 }
 
 /* Create a refresh packet for the given message ID.  You can assume
@@ -22,5 +148,20 @@ int pack(void *packed, char *input) {
  * Returns the packet type.
  */
 int pack_refresh(void *packed, int message_id) {
-    return -1;
-}
+
+    
+    char str[] = "dpang3\0\0\0\0\0\0\0\0\0\0"; // 16 bytes   
+    *(int *)packed = REFRESH; //0x0 to 0x3
+
+
+    //idk what im doing but.. packed plus 4 bytes plus i so we start at 0x3 plus whatever letter were on i guess
+    for(int i=0; i<NAME_SIZE; i++){
+        *(char *)(packed + (sizeof(int)+i)) = str[i]; 
+    }
+    
+    //address now starting at 0x20
+    *(int *)(packed + (5 * sizeof(int))) = message_id;
+
+ 
+
+    return *(int *)packed;
--- src/unpack.c
+++ src/unpack.c
@@ -12,8 +12,94 @@
  *
  * Returns the packet type that was unpacked, or -1 if it is invalid.
  */
+
+/* #define REFRESH    0
+#define STATUS     1
+#define MESSAGE    2
+#define LABELED    3
+#define STATISTICS 4
+*/
+
 int unpack(char *message, void *packed) {
-    return 0;
+    
+    if(*(int *)packed==3){
+        //labeled
+        //dpang3: @elb wassup, namelength=6
+        //0123456789abcdefghi
+        
+        int namelength=0;
+        for(int i=0; *(char *)(packed + (sizeof(int)+(i*sizeof(char))))!='\0'; i++){
+            message[i] = *(char *)(packed + (sizeof(int)+(i*sizeof(char))));
+            namelength++;
+        }
+        message[namelength]=':';
+        message[namelength+1]=' ';
+        message[namelength+2]='@';
+        //above prints "dpang3: @"
+
+
+        size_t messagelength = *(size_t *)(packed+(5*(sizeof(int))));
+        size_t targetlength = *(size_t *)(packed+(7*(sizeof(int))));
+        
+        for(int i=0; i<targetlength; i++){
+            message[namelength+3+i] = *(char *)(packed+ (44+messagelength+i) );
+        }
+
+        message[namelength+3+targetlength] = ' ';
+        //above prints "target "
+        //so far: "dpang3: @elb "
+        
+        for(int i=0; i<messagelength; i++){
+            message[namelength+4+targetlength+i] = *(char *)(packed+(44+i));
+        }
+        
+        return *(int *)packed;
+        //above prints "message"
+
+        
+    }
+
+    else if(*(int *)packed==1){
+        //status
+        if(*(size_t *)(packed+(5*sizeof(int)))==0){return -1;}
+        if(*(size_t *)(packed+(5*sizeof(int)))>MAX_MESSAGE_SIZE){return -1;}
+        //david says wassup namelength=5, message starts at index=7
+        int namelength=0;
+        for(int i=0; *(char *)(packed + (sizeof(int)+(i*sizeof(char))))!='\0'; i++){
+            message[i] = *(char *)(packed + (sizeof(int)+(i*sizeof(char))));
+            namelength++;
+        }
+        message[namelength]=' ';
+
+        
+        for(int i=0; i<*(size_t *)(packed+(5*sizeof(int))); i++){
+            message[namelength+1+i] = *(char *)(packed+(36+i));
+        }
+        return *(int *)packed;
+        
+        
+    }
+    else if(*(int *)packed==2){
+        //message
+        if(*(size_t *)(packed+(5*sizeof(int)))==0){return -1;}
+        if(*(size_t *)(packed+(5*sizeof(int)))>MAX_MESSAGE_SIZE){return -1;}
+        //david: wassup namelength=5, message starts at index=7
+        int namelength=0;
+        for(int i=0; *(char *)(packed + (sizeof(int)+(i*sizeof(char))))!='\0'; i++){
+            message[i] = *(char *)(packed + (sizeof(int)+(i*sizeof(char))));
+            namelength++;
+        }
+        message[namelength]=':';
+        message[namelength+1]=' ';
+        for(int i=0; i<*(size_t *)(packed+(5*sizeof(int))); i++){
+            message[namelength+2+i] = *(char *)(packed+(36+i));
+        }
+        return *(int *)packed;
+        
+    }
+
+    
+    return -1;
 }
 
 /* Unpack the given packed packet into the given statistics structure.
@@ -25,5 +111,28 @@ int unpack(char *message, void *packed) {
  * Returns the packet type that was unpacked, or -1 if it is invalid.
  */
 int unpack_statistics(struct statistics *statistics, void *packed) {
-    return 0;
-}
+
+    /*
+      if(){return -1;}*/
+
+    //statistics pack looks like by bytes: 4-16-16-4-8-8-4
+    statistics->messages_count = *(int *)(packed + 14); //int
+    statistics->invalid_count = *(long *)(packed + 5); //long
+    statistics->refresh_count = *(long *)(packed + 6); //long
+    statistics->most_active_count = *(int *)(packed + 9); //int
+
+    int i=0;
+    for(i=0; *(char *)(packed + (sizeof(int)+(i*sizeof(char))))!='\0'; i++){
+        statistics->sender[i] = *(char *)(packed + (sizeof(int) + i));
+        if(i==15){break;}
+    }
+    statistics->sender[i] = '\0';
+
+    i=0;
+    for(i=0; i<*(char *)(packed + (20 + (sizeof(char)*i)))!='\0'; i++){
+        statistics->most_active[i] = *(char *)(packed + (20 + i));
+        if(i==15){break;}
+    }
+    statistics->most_active[i] = '\0';
+
+    return *(int *)packed;
